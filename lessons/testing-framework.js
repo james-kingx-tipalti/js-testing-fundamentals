a9f134523941cd2967fe5d0d5ea14778acc60015
james.cabeso <james.cabeso@tipalti.com>
2025-05-16 16:14:33 +0100

    fundamentals of testing javascript chapter completed

--- lessons/testing-framework.js
+++ lessons/testing-framework.js
@@ -1,14 +1,26 @@
 const {sum, subtract} = require('../math')
 
+let result, expected
+
+
+function sumtest() {
+}
+
 test('sum adds numbers', () => {
   const result = sum(3, 7)
   const expected = 10
+
   expect(result).toBe(expected)
 })
 
+
+function subtractTest() {
+}
+
 test('subtract subtracts numbers', () => {
   const result = subtract(7, 3)
-  const expected = 4
+  const expected = 4 
+  
   expect(result).toBe(expected)
 })
 
@@ -17,7 +29,7 @@ function test(title, callback) {
     callback()
     console.log(`✓ ${title}`)
   } catch (error) {
-    console.error(`✕ ${title}`)
+    console.error(`✗ ${title}`)
     console.error(error)
   }
 }
@@ -25,9 +37,46 @@ function test(title, callback) {
 function expect(actual) {
   return {
     toBe(expected) {
-      if (actual !== expected) {
+      if(actual !== expected) {
         throw new Error(`${actual} is not equal to ${expected}`)
       }
-    }
+    },
+    toEqual(expected) {},
+    toBegreaterThan(expected) {},
   }
 }
+
+
+// const {sum, subtract} = require('../math')
+
+// test('sum adds numbers', () => {
+//   const result = sum(3, 7)
+//   const expected = 10
+//   expect(result).toBe(expected)
+// })
+
+// test('subtract subtracts numbers', () => {
+//   const result = subtract(7, 3)
+//   const expected = 4
+//   expect(result).toBe(expected)
+// })
+
+// function test(title, callback) {
+//   try {
+//     callback()
+//     console.log(`✓ ${title}`)
+//   } catch (error) {
+//     console.error(`✕ ${title}`)
+//     console.error(error)
+//   }
+// }
+
+// function expect(actual) {
+//   return {
+//     toBe(expected) {
+//       if (actual !== expected) {
+//         throw new Error(`${actual} is not equal to ${expected}`)
+//       }
+//     }
+//   }
+// }
--- setup-globals.js
+++ setup-globals.js
@@ -20,3 +20,26 @@ function expect(actual) {
 
 global.test = test
 global.expect = expect
+
+// async function test(title, callback) {
+//   try {
+//     await callback()
+//     console.log(`✓ ${title}`)
+//   } catch (error) {
+//     console.error(`✕ ${title}`)
+//     console.error(error)
+//   }
+// }
+
+// function expect(actual) {
+//   return {
+//     toBe(expected) {
+//       if (actual !== expected) {
+//         throw new Error(`${actual} is not equal to ${expected}`)
+//       }
+//     }
+//   }
+// }
+
+// global.test = test
+// global.expect = expect
